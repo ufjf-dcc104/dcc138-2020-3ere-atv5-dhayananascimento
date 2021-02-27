cb07f44f22ad3e35f3c21264036d5a91d06e7935
Dhayana N. Silva <dhayana@ice.ufjf.br>
2021-02-27 19:23:50 -0300

    Cria arquivo de gerenciador de recursos

--- js/Main.js
+++ js/Main.js
@@ -1,9 +1,28 @@
 import Cena from "./Cena.js";
 import Sprite from "./Sprite.js";
+import AssetManager from "./AssetsManager.js";
+
+const img1 = new Image();
+img1.src = "./assets/girl.png";
+img1.alt = "girl";
+
+const img2 = new Image();
+img2.src = "./assets/orc.png";
+img2.alt = "orc";
+
+const img3 = new Image();
+img3.src = "./assets/skeleton.png";
+img3.alt = "skeleton";
+
+document.body.appendChild(img1);
+document.body.appendChild(img2);
+document.body.appendChild(img3);
+
+const assets = new AssetManager();
 
 const canvas = document.querySelector("canvas");
 
-const cena1 = new Cena(canvas);
+const cena1 = new Cena(canvas, assets);
 
 const pc = new Sprite({ vx: 10 });
 const en1 = new Sprite({ x: 140, w: 30, color: "red" });
