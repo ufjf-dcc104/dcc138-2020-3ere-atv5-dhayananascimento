fffef1119ea76b8bb01b1e03b6b22a6027de450b
Dhayana N. Silva <dhayana@ice.ufjf.br>
2021-03-03 01:50:05 -0300

    Cria um mapa para uso na Cena

--- js/Cena.js
+++ js/Cena.js
@@ -10,11 +10,14 @@ export default class Cena {
     this.dt = 0;
     this.idAnim = null;
     this.assets = assets;
+    this.mapa = null;
   }
 
   desenhar() {
-    this.ctx.fillStyle = "grey";
+    this.ctx.fillStyle = "lightblue";
     this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+    this.mapa?.desenhar(this.ctx);
 
     if (this.assets.acabou()) {
       for (let s = 0; s < this.sprites.length; s++) {
@@ -87,5 +90,10 @@ export default class Cena {
     }
 
     this.aRemover = [];
+  }
+
+  configuraMapa(mapa) {
+    this.mapa = mapa;
+    this.mapa.cena = this;
   }
 }
