1394ccc07bb27e4b0dd2da784b7b04da5bb9b291
Dhayana N. Silva <dhayana@ice.ufjf.br>
2021-03-17 01:34:16 -0300

    Adiciona perseguição ao inimigo

--- js/Main.js
+++ js/Main.js
@@ -42,137 +42,156 @@ const mapa = new Mapa(ALTURA_MAPA, LARGURA_MAPA, TAMANHO_TILE);
 mapa.carregaMapa(modeloMapa);
 cena.configuraMapa(mapa);
 
-function criaSprite() {
-  let sprite_x, sprite_y;
-  const sprite_vx =
-    Math.random() * (VELOCIDADE_SPRITE - -VELOCIDADE_SPRITE) +
-    -VELOCIDADE_SPRITE;
-  const sprite_vy =
-    Math.random() * (VELOCIDADE_SPRITE - -VELOCIDADE_SPRITE) +
-    -VELOCIDADE_SPRITE;
+// function criaSprite() {
+//   let sprite_x, sprite_y;
+//   const sprite_vx =
+//     Math.random() * (VELOCIDADE_SPRITE - -VELOCIDADE_SPRITE) +
+//     -VELOCIDADE_SPRITE;
+//   const sprite_vy =
+//     Math.random() * (VELOCIDADE_SPRITE - -VELOCIDADE_SPRITE) +
+//     -VELOCIDADE_SPRITE;
 
-  let posicaoMapaLinhaEsquerdaTopo,
-    posicaoMapaColunaEsquerdaTopo,
-    posicaoMapaLinhaEsquerdaBaixo,
-    posicaoMapaColunaEsquerdaBaixo;
+//   let posicaoMapaLinhaEsquerdaTopo,
+//     posicaoMapaColunaEsquerdaTopo,
+//     posicaoMapaLinhaEsquerdaBaixo,
+//     posicaoMapaColunaEsquerdaBaixo;
 
-  let posicaoMapaLinhaDireitaTopo,
-    posicaoMapaColunaDireitaTopo,
-    posicaoMapaLinhaDireitaBaixo,
-    posicaoMapaColunaDireitaBaixo;
+//   let posicaoMapaLinhaDireitaTopo,
+//     posicaoMapaColunaDireitaTopo,
+//     posicaoMapaLinhaDireitaBaixo,
+//     posicaoMapaColunaDireitaBaixo;
 
-  let valorMapaEsquerdaTopo,
-    valorMapaEsquerdaBaixo,
-    valorMapaDireitaTopo,
-    valorMapaDireitaBaixo;
+//   let valorMapaEsquerdaTopo,
+//     valorMapaEsquerdaBaixo,
+//     valorMapaDireitaTopo,
+//     valorMapaDireitaBaixo;
 
-  do {
-    sprite_x = Math.floor(
-      Math.random() * ((LARGURA_MAPA - 1) * TAMANHO_TILE - TAMANHO_TILE) +
-        TAMANHO_TILE
-    );
-    sprite_y = Math.floor(
-      Math.random() * ((ALTURA_MAPA - 1) * TAMANHO_TILE - TAMANHO_TILE) +
-        TAMANHO_TILE
-    );
+//   do {
+//     sprite_x = Math.floor(
+//       Math.random() * ((LARGURA_MAPA - 1) * TAMANHO_TILE - TAMANHO_TILE) +
+//         TAMANHO_TILE
+//     );
+//     sprite_y = Math.floor(
+//       Math.random() * ((ALTURA_MAPA - 1) * TAMANHO_TILE - TAMANHO_TILE) +
+//         TAMANHO_TILE
+//     );
 
-    sprite_x -= TAMANHO_SPRITE / 2;
-    sprite_y -= TAMANHO_SPRITE / 2;
+//     sprite_x -= TAMANHO_SPRITE / 2;
+//     sprite_y -= TAMANHO_SPRITE / 2;
 
-    const linhaTopo =
-      sprite_y % TAMANHO_TILE == 0
-        ? sprite_y / TAMANHO_TILE - 1
-        : sprite_y / TAMANHO_TILE;
+//     const linhaTopo =
+//       sprite_y % TAMANHO_TILE == 0
+//         ? sprite_y / TAMANHO_TILE - 1
+//         : sprite_y / TAMANHO_TILE;
 
-    const linhaBaixo =
-      (sprite_y + TAMANHO_SPRITE) % TAMANHO_TILE == 0
-        ? (sprite_y + TAMANHO_SPRITE) / TAMANHO_TILE - 1
-        : (sprite_y + TAMANHO_SPRITE) / TAMANHO_TILE;
+//     const linhaBaixo =
+//       (sprite_y + TAMANHO_SPRITE) % TAMANHO_TILE == 0
+//         ? (sprite_y + TAMANHO_SPRITE) / TAMANHO_TILE - 1
+//         : (sprite_y + TAMANHO_SPRITE) / TAMANHO_TILE;
 
-    const colunaEsquerda =
-      sprite_x % TAMANHO_TILE == 0
-        ? sprite_x / TAMANHO_TILE - 1
-        : sprite_x / TAMANHO_TILE;
+//     const colunaEsquerda =
+//       sprite_x % TAMANHO_TILE == 0
+//         ? sprite_x / TAMANHO_TILE - 1
+//         : sprite_x / TAMANHO_TILE;
 
-    const colunaDireita =
-      (sprite_x + TAMANHO_SPRITE) % TAMANHO_TILE == 0
-        ? (sprite_x + TAMANHO_SPRITE) / TAMANHO_TILE - 1
-        : (sprite_x + TAMANHO_SPRITE) / TAMANHO_TILE;
+//     const colunaDireita =
+//       (sprite_x + TAMANHO_SPRITE) % TAMANHO_TILE == 0
+//         ? (sprite_x + TAMANHO_SPRITE) / TAMANHO_TILE - 1
+//         : (sprite_x + TAMANHO_SPRITE) / TAMANHO_TILE;
 
-    posicaoMapaLinhaEsquerdaTopo = Math.floor(linhaTopo);
-    posicaoMapaColunaEsquerdaTopo = Math.floor(colunaEsquerda);
+//     posicaoMapaLinhaEsquerdaTopo = Math.floor(linhaTopo);
+//     posicaoMapaColunaEsquerdaTopo = Math.floor(colunaEsquerda);
 
-    posicaoMapaLinhaEsquerdaBaixo = Math.floor(linhaBaixo);
-    posicaoMapaColunaEsquerdaBaixo = Math.floor(colunaEsquerda);
+//     posicaoMapaLinhaEsquerdaBaixo = Math.floor(linhaBaixo);
+//     posicaoMapaColunaEsquerdaBaixo = Math.floor(colunaEsquerda);
 
-    posicaoMapaLinhaDireitaTopo = Math.floor(linhaTopo);
-    posicaoMapaColunaDireitaTopo = Math.floor(colunaDireita);
+//     posicaoMapaLinhaDireitaTopo = Math.floor(linhaTopo);
+//     posicaoMapaColunaDireitaTopo = Math.floor(colunaDireita);
 
-    posicaoMapaLinhaDireitaBaixo = Math.floor(linhaBaixo);
-    posicaoMapaColunaDireitaBaixo = Math.floor(colunaDireita);
+//     posicaoMapaLinhaDireitaBaixo = Math.floor(linhaBaixo);
+//     posicaoMapaColunaDireitaBaixo = Math.floor(colunaDireita);
 
-    valorMapaEsquerdaTopo =
-      modeloMapa?.[posicaoMapaLinhaEsquerdaTopo]?.[
-        posicaoMapaColunaEsquerdaTopo
-      ];
-    valorMapaEsquerdaBaixo =
-      modeloMapa?.[posicaoMapaLinhaEsquerdaBaixo]?.[
-        posicaoMapaColunaEsquerdaBaixo
-      ];
-    valorMapaDireitaTopo =
-      modeloMapa?.[posicaoMapaLinhaDireitaTopo]?.[posicaoMapaColunaDireitaTopo];
-    valorMapaDireitaBaixo =
-      modeloMapa?.[posicaoMapaLinhaDireitaBaixo]?.[
-        posicaoMapaColunaDireitaBaixo
-      ];
-  } while (
-    valorMapaEsquerdaTopo === 1 ||
-    valorMapaEsquerdaTopo === 2 ||
-    valorMapaEsquerdaTopo === undefined ||
-    valorMapaEsquerdaBaixo === 1 ||
-    valorMapaEsquerdaBaixo === 2 ||
-    valorMapaEsquerdaBaixo === undefined ||
-    valorMapaDireitaTopo === 1 ||
-    valorMapaDireitaTopo === 2 ||
-    valorMapaDireitaTopo === undefined ||
-    valorMapaDireitaBaixo === 1 ||
-    valorMapaDireitaBaixo === 2 ||
-    valorMapaDireitaBaixo === undefined
-  );
+//     valorMapaEsquerdaTopo =
+//       modeloMapa?.[posicaoMapaLinhaEsquerdaTopo]?.[
+//         posicaoMapaColunaEsquerdaTopo
+//       ];
+//     valorMapaEsquerdaBaixo =
+//       modeloMapa?.[posicaoMapaLinhaEsquerdaBaixo]?.[
+//         posicaoMapaColunaEsquerdaBaixo
+//       ];
+//     valorMapaDireitaTopo =
+//       modeloMapa?.[posicaoMapaLinhaDireitaTopo]?.[posicaoMapaColunaDireitaTopo];
+//     valorMapaDireitaBaixo =
+//       modeloMapa?.[posicaoMapaLinhaDireitaBaixo]?.[
+//         posicaoMapaColunaDireitaBaixo
+//       ];
+//   } while (
+//     valorMapaEsquerdaTopo === 1 ||
+//     valorMapaEsquerdaTopo === 2 ||
+//     valorMapaEsquerdaTopo === undefined ||
+//     valorMapaEsquerdaBaixo === 1 ||
+//     valorMapaEsquerdaBaixo === 2 ||
+//     valorMapaEsquerdaBaixo === undefined ||
+//     valorMapaDireitaTopo === 1 ||
+//     valorMapaDireitaTopo === 2 ||
+//     valorMapaDireitaTopo === undefined ||
+//     valorMapaDireitaBaixo === 1 ||
+//     valorMapaDireitaBaixo === 2 ||
+//     valorMapaDireitaBaixo === undefined
+//   );
 
-  const sprite = new Sprite({
-    x: sprite_x + TAMANHO_SPRITE / 2,
-    y: sprite_y + TAMANHO_SPRITE / 2,
-    // vx: sprite_vx,
-    // vy: sprite_vy,
-    color: "red",
-  });
+//   const sprite = new Sprite({
+//     x: sprite_x + TAMANHO_SPRITE / 2,
+//     y: sprite_y + TAMANHO_SPRITE / 2,
+//     vx: sprite_vx,
+//     vy: sprite_vy,
+//     color: "red",
+//   });
 
-  sprite.controlar = function (dt) {
-    if (input.comandos.get("MOVE_ESQUERDA")) {
-      this.vx = -50;
-    } else if (input.comandos.get("MOVE_DIREITA")) {
-      this.vx = 50;
-    } else {
-      this.vx = 0;
-    }
+//   cena.adicionar(sprite);
+// }
 
-    if (input.comandos.get("MOVE_CIMA")) {
-      this.vy = -50;
-    } else if (input.comandos.get("MOVE_BAIXO")) {
-      this.vy = 50;
-    } else {
-      this.vy = 0;
-    }
-  };
+const pc = new Sprite({
+  x: 80,
+  y: 50,
+});
+cena.adicionar(pc);
+pc.controlar = movimentaSprite;
 
-  cena.adicionar(sprite);
+const en1 = new Sprite({
+  x: 220,
+  y: 200,
+  color: "red",
+});
+cena.adicionar(en1);
+en1.controlar = persegueSprite;
+
+function persegueSprite(dt) {
+  this.vx = 25 * Math.sign(pc.x - this.x);
+  this.vy = 25 * Math.sign(pc.y - this.y);
 }
 
-criaSprite();
+function movimentaSprite(dt) {
+  if (input.comandos.get("MOVE_ESQUERDA")) {
+    this.vx = -50;
+  } else if (input.comandos.get("MOVE_DIREITA")) {
+    this.vx = 50;
+  } else {
+    this.vx = 0;
+  }
+
+  if (input.comandos.get("MOVE_CIMA")) {
+    this.vy = -50;
+  } else if (input.comandos.get("MOVE_BAIXO")) {
+    this.vy = 50;
+  } else {
+    this.vy = 0;
+  }
+}
 
 cena.iniciar();
 
+// criaSprite("jogador");
 // setInterval(criaSprite, 4000);
 
 document.addEventListener("keydown", (e) => {
--- js/Sprite.js
+++ js/Sprite.js
@@ -9,6 +9,7 @@ export default class Sprite {
     w = 20,
     h = 20,
     color = "white",
+    controlar = () => {},
   } = {}) {
     this.x = x;
     this.y = y;
@@ -20,6 +21,7 @@ export default class Sprite {
     this.cena = null;
     this.mx = 0;
     this.my = 0;
+    this.controlar = controlar;
   }
 
   desenhar(ctx) {
